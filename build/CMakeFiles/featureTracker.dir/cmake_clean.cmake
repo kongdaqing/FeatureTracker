3c3a3b678942069ff68e22215fd612f95c3dd4df
zhangxing <zhangxing@zerozero.cn>
2019-12-11 10:51:27 +0800

    first build project

---
+++ build/CMakeFiles/featureTracker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/featureTracker.dir/src/AnkerInterface.cpp.o"
+  "featureTracker"
+  "featureTracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/featureTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
